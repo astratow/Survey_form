b6f80246b67972e574b30cc0a805c7b8918fcf71
astratow <davno_pora@mail.com>
2021-08-05 05:04:11 +0100

    added thank you, original index.html file renamed to index-old.html

---
+++ app.js
@@ -0,0 +1,8 @@
+function results() {
+	var name = document.getElementById("name").value;
+	var emal = document.getElementById("email").value;
+	var age = document.getElementById("age").value;
+	
+	document.write("<h1>Thank you for submiting survey!</h1>");
+	
+}
