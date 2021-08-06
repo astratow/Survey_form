a8c3438273b4e1503a9dee170c549bf1ad527c56
astratow <davno_pora@mail.com>
2021-08-06 05:42:41 +0100

    attempt to add stylesheet. Failed

--- app.js
+++ app.js
@@ -2,7 +2,10 @@ function results() {
 	var name = document.getElementById("name").value;
 	var emal = document.getElementById("email").value;
 	var age = document.getElementById("age").value;
-	
+	var link = document.createElement("link");
+	link.setAttribute("rel", "stylesheet");
+	link.setAttribute("href", "style.css");
+	document.getElementsByTagName("head")[0].appendChild(link);
 	document.write("<h1>Thank you for submiting survey!</h1>");
 	
 }
